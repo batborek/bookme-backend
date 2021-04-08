2f5e530a9efb0848bbce8fe986d3ed905d59b598
Perman Yagmyrov <permanyagmyrov@gmail.com>
2021-04-08 11:37:15 +0100

    edited some conf files

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -3,9 +3,9 @@
 set -e
 
 # TODO: Set to URL of git repo.
-PROJECT_GIT_URL='https://github.com/CHANGEME.git'
+PROJECT_GIT_URL='https://github.com/batborek/bookme-backend.git'
 
-PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
+PROJECT_BASE_PATH='/usr/local/apps/bookme-api'
 
 echo "Installing dependencies..."
 apt-get update
@@ -29,15 +29,15 @@ $PROJECT_BASE_PATH/env/bin/python manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
 
 # Configure supervisor
-cp $PROJECT_BASE_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
+cp $PROJECT_BASE_PATH/deploy/supervisor_bookme-api.conf /etc/supervisor/conf.d/bookme_api.conf
 supervisorctl reread
 supervisorctl update
-supervisorctl restart profiles_api
+supervisorctl restart bookme-api
 
 # Configure nginx
-cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
+cp $PROJECT_BASE_PATH/deploy/nginx_bookme_api.conf /etc/nginx/sites-available/bookme_api.conf
 rm /etc/nginx/sites-enabled/default
-ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
+ln -s /etc/nginx/sites-available/bookme_api.conf /etc/nginx/sites-enabled/bookme_api.conf
 systemctl restart nginx.service
 
 echo "DONE! :)"
