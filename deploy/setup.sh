232b3dd7d6db5597cdc8be1857f8e52345676cb1
Perman Yagmyrov <permanyagmyrov@gmail.com>
2021-04-09 02:50:09 +0100

    user

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -5,7 +5,7 @@ set -e
 # TODO: Set to URL of git repo.
 PROJECT_GIT_URL='https://github.com/batborek/bookme-backend.git'
 
-PROJECT_BASE_PATH='/usr/local/apps/bookme'
+PROJECT_BASE_PATH='/usr/local/apps/bookme_api'
 
 echo "Installing dependencies..."
 apt-get update
--- deploy/update.sh
+++ deploy/update.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-PROJECT_BASE_PATH='/usr/local/apps/bookme'
+PROJECT_BASE_PATH='/usr/local/apps/bookme_api'
 
 git pull
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
