c83d0a31e5049f2a2b7f8e3a074e37be7f7d686a
Perman Yagmyrov <permanyagmyrov@gmail.com>
2021-04-09 06:32:20 +0100

    added admin

--- bookme_api/admin.py
+++ bookme_api/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 
 from bookme_api import models
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'name', 'is_staff',)
+    list_filter = ('is_staff',)
+    search_fields = ('email',)
+    list_per_page = 50
+
+admin.site.register(models.UserProfile, UserAdmin)
 admin.site.register(models.Room)
 # Register your models here.
